65ec0fcdb99065c771d8d1c6d0c7cdb20d94f2e3
vikram rathode <vikramrambhad25@gmail.com>
2022-03-02 01:06:19 +0530

    vikram

--- pages/component/ProductFeed.js
+++ pages/component/ProductFeed.js
@@ -4,7 +4,7 @@ function ProductFeed({ products }) {
     console.log(products);
     return (
         <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
-            {products.map(({ id, title, price, description, category, image }) => (
+            {products.slice(0,4).map(({ id, title, price, description, category, image }) => (
                 <Product
                     key={id}
                     title={title}
@@ -14,7 +14,7 @@ function ProductFeed({ products }) {
                     image={image}
                 />
             ))}
-            {/* <img className="md:col-span-full" src="https://links.papareact.com/dyz" alt="" />
+            <img className="md:col-span-full" src="https://links.papareact.com/dyz" alt="" />
             <div className="md:col-span-2 ">
             {products.slice(4,5).map(({ id, title, price, description, category, image }) => (
                 <Product
@@ -36,7 +36,7 @@ function ProductFeed({ products }) {
                     category={category}
                     image={image}
                 />
-            ))} */}
+            ))}
         </div>
     );
 }
